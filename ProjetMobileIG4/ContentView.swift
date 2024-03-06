f3da4d184ef798d587a350b34ef7f0d85b8ec85b
Robin Vincent <robin.vincent@etu.umontpellier.fr>
2024-03-06 16:20:40 +0100

    Initial Commit

---
+++ ProjetMobileIG4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjetMobileIG4
+//
+//  Created by Robin Vincent on 06/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjetMobileIG4/ProjetMobileIG4App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetMobileIG4App.swift
+//  ProjetMobileIG4
+//
+//  Created by Robin Vincent on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjetMobileIG4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
