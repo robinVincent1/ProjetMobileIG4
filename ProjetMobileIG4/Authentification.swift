c1e24e7f637a0ca74e3a5dbab660bebd89d5aac5
Robin Vincent <robin.vincent@etu.umontpellier.fr>
2024-03-11 16:30:13 +0100

    Authentification + login + register

---
+++ ProjetMobileIG4/Authentification.swift
@@ -0,0 +1,8 @@
+//
+//  Authentification.swift
+//  ProjetMobileIG4
+//
+//  Created by Robin Vincent on 11/03/2024.
+//
+
+import Foundation
---
+++ ProjetMobileIG4/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  ProjetMobileIG4
+//
+//  Created by Robin Vincent on 09/03/2024.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
